85651dc75e254cdfcf95e2393cc8073422f9672c
Anthony Fieroni <bvbfan@abv.bg>
2020-09-03 13:51:50 +0300

    First commit

---
+++ bitwise_add.cpp
@@ -0,0 +1,40 @@
+#include <assert.h>
+#include <iostream>
+#include <limits.h>
+
+/**
+ * Function to add to integers with bitwise operatrions
+ * in case of overflow function returns 0
+ * @param[in]  a         integer
+ * @param[in]  b         integer
+ */
+int add(int a, int b)
+{
+    // found out overflow in two negative ints
+    if (a < 0 && b < 0 && !((a & b) & INT_MAX))
+        return 0; // overflow
+
+    bool overflow = a >= 0 && b >= 0;
+    while (b != 0) {
+        int carry = a & b;
+        // find out overflow in two positive ints
+        if (overflow && ((carry >> (sizeof(a) * 8 -2) & 1)))
+            return 0; // overflow
+        a ^= b;
+        b = carry << 1;
+    }
+    return a;
+}
+
+int main()
+{
+    assert(add(5, 6) == 11 && "5 + 6 is 11");
+    assert(add(-1, 6) == 5 && "-1 + 6 is 5");
+    assert(add(-1, -6) == -7 && "-1 + -6 is -7");
+    assert(add(INT_MAX, 1) == 0 && "INT_MAX +1 is overflow");
+    assert(add(INT_MIN, 1) == INT_MIN+1 && "INT_MIN -1 is overflow");
+    assert(add(INT_MAX-1, 1) == INT_MAX && "INT_MAX-1+1 is INT_MAX");
+    assert(add(INT_MIN+2, -2) == INT_MIN && "INT_MIN+2 -2 is INT_MIN");
+    std::cout << "Tests finishes successfully!\n";
+    return 0;
+}
---
+++ subset_sum.cpp
@@ -0,0 +1,94 @@
+
+#include <algorithm>
+#include <assert.h>
+#include <iostream>
+#include <limits.h>
+#include <numeric>
+#include <vector>
+
+/**
+ * Function to find subset with largest sum less than or equal to T
+ * @param[in]  T         integer less or equal to
+ * @param[in]  I         integer set to look in
+ * @param[out] M         integer subset containg elements of largest sum
+ * @param[out] S         integer the largest subset sum
+ */
+void find_largest_sum(uint32_t T, const std::vector<uint32_t> &I,
+                      std::vector<uint32_t> &M, uint32_t &S)
+{
+    if (I.empty())
+        return;
+
+    auto start = I.begin();
+    std::vector<uint32_t> work;
+    auto sum = *start > T ? 0 : *start;
+    if (sum)
+        work.push_back(sum);
+    S = 0;
+
+    // To find sum less or equal to T
+    for (auto it = start + 1; it != I.end(); ++it) {
+
+        if (*it > T)
+            continue;
+
+        // If sum becomes greater than T
+        // remove starting from start till new one fits
+        // prefer continuous subset over non-continuous
+        while (((sum > UINT_MAX - *it) || (sum + *it > T)) && start != it) {
+            if (S < sum) {
+                S = sum;
+                M = work;
+            }
+            sum -= *start;
+            work.erase(std::find(work.begin(), work.end(), *start));
+            ++start;
+        }
+
+        // Add element to sum
+        sum += *it;
+        work.push_back(*it);
+    }
+
+    if (S < sum) {
+        S = sum;
+        M = work;
+    }
+}
+
+// Driver program to test above function
+int main()
+{
+    uint32_t sum;
+    std::vector<uint32_t> arr, b;
+
+    arr = {1, 2, 3, 4, 5, 6, 7};
+    sum = 11;
+
+    find_largest_sum(sum, arr, b, sum);
+
+    assert(sum == 11 && "Sum does not match");
+    assert(b.size() == 2 && "Result size does not match");
+    assert(std::equal(b.begin(), b.end(), std::vector<uint32_t>{5, 6}.begin()) && "Result array does not match");
+
+    arr = {6, 5, 9, 3, 10, 8, 7};
+    sum = 18;
+
+    find_largest_sum(sum, arr, b, sum);
+
+    assert(sum == 18 && "Sum does not match");
+    assert(b.size() == 2 && "Result size does not match");
+    assert(std::equal(b.begin(), b.end(), std::vector<uint32_t>{10, 8}.begin()) && "Result array does not match");
+
+    arr = {6, 2, 4, 10, 8, 9};
+    sum = 15;
+
+    find_largest_sum(sum, arr, b, sum);
+
+    assert(sum == 14 && "Sum does not match");
+    assert(b.size() == 2 && "Result size does not match");
+    assert(std::equal(b.begin(), b.end(), std::vector<uint32_t>{4, 10}.begin()) && "Result array does not match");
+
+    std::cout << "Tests finishes successfully!\n";
+    return 0;
+}
